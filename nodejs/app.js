e9b045da0bfc7af06126db4d2acbad2815715a3b
Diomede <diomede.mazzone@gmail.com>
2018-02-27 22:30:42 +0100

    correzioni di bug

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const app = express();
-const io = require('socket.io');
+
 const log = require('morgan');
 const path = require('path');
 
 app.use(log ('dev') );
 
-app.use( function (rq, res, next){
+app.use( function (req, res, next){
 	console.log('Richiesta verso ' + req.url + '(' + new Date() + ')');
 	next();
 });
 
+app.use(
+	'/js',
+	express.static ( path.resolve( __dirname, 'js'))
+);
 
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname, 'html', 'joy.html');
+  res.sendFile(
+  		path.resolve( __dirname, 'html', 'basic.html')
+  		);
 });
 
-
-io.on('connection', function (socket) {
-  console.log('richiesta di connessione dal client');
-  console.log('invio tutte le temperature');
-
-  });
-});
 
 
 app.listen(3000, function() {
